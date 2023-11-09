8b74830f1a6f7ac1c643e355814be1b03d975779
nezz0746 <nezz0746@gmail.com>
2023-11-09 08:23:15 +0100

    broken mumbai rpc url

--- apps/web/viem_utils.ts
+++ apps/web/viem_utils.ts
@@ -11,7 +11,7 @@ if (!mumbaiApiKey) {
   console.log("MUMBAI_ALCHEMY_KEY: ", mumbaiApiKey);
 }
 
-const mumbaiRpcUrl = `https://eth-goerli.g.alchemy.com/v2/${mumbaiApiKey}`;
+const mumbaiRpcUrl = `${polygonMumbai.rpcUrls.alchemy.http}/${mumbaiApiKey}`;
 const account_key = `0x${process.env.RELAYER_PRIVATE_KEY}` as `0x${string}`;
 const defaultChainId = 80001;
 
