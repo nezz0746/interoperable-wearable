e015c1d2840f0e47614129b88a78424f2c7ac22c
nezz0746 <nezz0746@gmail.com>
2023-11-15 01:03:22 +0100

    feat: add route for TBA metadata w/ animation_url

---
+++ apps/web/app/api/metadata/[tokenId]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = {
+  params: { tokenId: string };
+};
+
+const url =
+  process.env.NODE_ENV === "development"
+    ? "localhost:3001"
+    : process.env.VERCEL_URL;
+
+const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+export async function GET(req: NextRequest, { params: { tokenId } }: Params) {
+  return NextResponse.json({
+    name: "Gaian Wearable Pack",
+    description: "Gaian Pack: Includes 5 wearable.",
+    image: "ipfs://bafkreifukc4gawybgl4736ltkwwoqoaxcl77r45yyf56u2qxu6xodutw2y",
+    animation_url: `${protocol}://${url}/iframe/${tokenId}`,
+  });
+}
